0a49f4ce2d8f44cf818d3057aa390e62ec6b1369
Brayan6072 <brayan.delgado6072@alumnos.udg.mx>
2024-04-14 20:29:16 -0600

    Reportes y Login

--- N.sql
+++ N.sql
@@ -23,16 +23,20 @@ USE `notificaciones`;
 CREATE TABLE IF NOT EXISTS `reportes` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Fecha` date DEFAULT NULL,
-  `EtiquetaU` varchar(50) ,
+  `EtiquetaU` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Hora` time DEFAULT NULL,
-  `EstdBote` varchar(50) ,
+  `EstdBote` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Clasificacion` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Volcando datos para la tabla notificaciones.reportes: ~1 rows (aproximadamente)
 INSERT INTO `reportes` (`Id`, `Fecha`, `EtiquetaU`, `Hora`, `EstdBote`, `Clasificacion`) VALUES
-	(1, '2024-04-14', 'Bote B', '13:22:13', 'Lleno', 'Plasticos');
+	(1, '2024-04-14', 'Bote B', '13:22:13', 'Lleno', 'Plasticos'),
+	(2, '2024-04-14', 'Bote O', '20:59:20', 'Lleno', 'Metal'),
+	(3, '2024-04-14', 'Bote T', '20:59:26', 'Basura Mezclada', 'Metal'),
+	(4, '2024-04-14', 'Bote M', '20:59:30', 'Basura Mezclada', 'Papel'),
+	(5, '2024-04-14', 'Bote J', '21:19:14', 'Lleno', 'Papel');
 
 -- Volcando estructura para tabla notificaciones.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -44,11 +48,13 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `TpUser` varchar(80) DEFAULT NULL,
   `Estado` varchar(50) DEFAULT 'Activo',
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla notificaciones.usuarios: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla notificaciones.usuarios: ~3 rows (aproximadamente)
 INSERT INTO `usuarios` (`Id`, `Nombre`, `Email`, `Telefono`, `Password`, `TpUser`, `Estado`) VALUES
-	(1, 'Brayan6072', 'brayan.delgado6072@alumnos.udg.mx', 123456789, '123456', 'Encargado', 'Activo');
+	(1, 'Brayan6072', 'brayan.delgado6072@alumnos.udg.mx', 123456789, '123456', 'Encargado', 'Activo'),
+	(2, 'Brayan ', 'ben10delgado@gmail.com', 123456, '123', 'Encargado', 'Activo'),
+	(3, 'Brayan1', 'brayandelgadodiaz03@gmail.com', 12316546, '123', 'Encargado', 'Activo');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
